af56ea1b1f6c16ca8f4250afc018579d25e831f1
Narupalle_swapna <nswapna.fullstack@gmail.com>
2025-09-02 13:38:43 +0530

    HTTP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular_HTTP';
+export class AppComponent implements OnInit {
+  // title = 'Angular_HTTP';
+
+  jsonAPI = "https://jsonplaceholder.typicode.com/todos";
+  data:any = []
+
+  constructor(private httpClient:HttpClient){
+
+  }
+
+  //To transform the data
+  //subscribe And initialise
+
+  ngOnInit(){
+     
+    let endPoint = this.httpClient.get(this.jsonAPI);
+           endPoint.subscribe((data:any)=>{
+            console.log("Angular_HTTP is: ", data);
+            this.data=data;
+           })
+  }
+
+
 }
